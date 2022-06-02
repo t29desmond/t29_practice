b96e0fe3cc18a32c2edaf7ff05e50d4317403774
윤대식 <t29desmond@gmail.com>
2022-06-02 22:17:45 +0900

    mozzet Member

---
+++ 01_component_practice/01_component_practice/MemberInfo.swift
@@ -0,0 +1,30 @@
+//
+//  MemberInfo.swift
+//  01_component_practice
+//
+//  Created by 윤대식 on 2022/06/02.
+//
+
+import UIKit
+
+struct MozzetMemberInfo {
+  var name: String
+  var career: String
+  var photo: String
+}
+
+var mozzetMemberInfo: [MozzetMemberInfo] = [
+  MozzetMemberInfo(name: "SY.KWON", career: "CEO", photo: "01.png"),
+  MozzetMemberInfo(name: "YH.CHO", career: "Designer", photo: "02.png"),
+  MozzetMemberInfo(name: "EY.YANG", career: "Operating Part Leader", photo: "03.png"),
+  MozzetMemberInfo(name: "MK.PARK", career: "CTO", photo: "04.png"),
+  MozzetMemberInfo(name: "DALKONG", career: "인턴", photo: "05.png"),
+  MozzetMemberInfo(name: "JS.YU", career: "Marketer", photo: "06.png"),
+  MozzetMemberInfo(name: "SH.KIM", career: "Service Operater", photo: "07.png"),
+  MozzetMemberInfo(name: "MY.JO", career: "Android Part Leader", photo: "08.png"),
+  MozzetMemberInfo(name: "SC.KIM", career: "​iOS Developer", photo: "09.png"),
+  MozzetMemberInfo(name: "DW.CHOI", career: "Server Developer", photo: "10.png"),
+  MozzetMemberInfo(name: "SH.WON", career: "Accountant", photo: "11.png"),
+  MozzetMemberInfo(name: "JS.LEE", career: "Designer", photo: "12.png"),
+  MozzetMemberInfo(name: "DH.YU", career: "Android Developer", photo: "13.png")
+]
--- 01_component_practice/01_component_practice/ViewController.swift
+++ 01_component_practice/01_component_practice/ViewController.swift
@@ -9,8 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var countNum: Int = 0
+
+  @IBOutlet weak var memberName: UILabel!
+  @IBOutlet weak var memberCareer: UILabel!
+
+  @IBOutlet weak var memberPhoto: UIImageView!
+  @IBOutlet weak var memberPhotoBar: UIView!
+
+  @IBOutlet weak var previousLabel: UIButton!
+  @IBOutlet weak var nextLabel: UIButton!
+
   override func viewDidLoad() {
+
     super.viewDidLoad()
+    view.backgroundColor = .systemGray6
+
+    self.memberPhoto.layer.cornerRadius = 28
+    self.memberPhotoBar.backgroundColor = .systemGray4
+
+    self.memberPhotoBar.layer.cornerRadius = 26
+    self.memberPhotoBar.layer.maskedCorners = CACornerMask(arrayLiteral:
+        .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
+
+    self.memberPhoto.image = UIImage(named: mozzetMemberInfo[0].photo)
+    self.memberName.text = mozzetMemberInfo[0].name
+    self.memberCareer.text = mozzetMemberInfo[0].career
+
+    self.memberName.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+    self.memberCareer.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+
+    self.previousLabel.setTitle("이전", for: .normal)
+    self.nextLabel.setTitle("다음", for: .normal)
+
+  }
+
+
+  @IBAction func previousButton(_ sender: UIButton) {
+    countNum = countNum - 1
+
+    if countNum < 0 {
+      self.countNum = mozzetMemberInfo.count - 1
+    }
+    self.memberPhoto.image = UIImage(named: mozzetMemberInfo[countNum].photo)
+    self.memberName.text = mozzetMemberInfo[countNum].name
+    self.memberCareer.text = mozzetMemberInfo[countNum].career
+  }
+
+
+  @IBAction func nextButton(_ sender: UIButton) {
+    countNum = countNum + 1
+
+    if countNum < 0 {
+      self.countNum = mozzetMemberInfo.count + 1
+    } else if countNum == 13 {
+      countNum = 0
+  }
+    self.memberPhoto.image = UIImage(named: mozzetMemberInfo[countNum].photo)
+    self.memberName.text = mozzetMemberInfo[countNum].name
+    self.memberCareer.text = mozzetMemberInfo[countNum].career
   }
 }
 
