34eacc28858d230e8390f2f19d088b9f5f0ade78
t29desmond <t29desmond@gmail.com>
2022-06-02 16:37:05 +0900

    first Commit

---
+++ 01_component_practice/01_component_practice/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  01_component_practice
+//
+//  Created by 윤대식 on 2022/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
